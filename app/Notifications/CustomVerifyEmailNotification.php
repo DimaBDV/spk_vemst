6fb32ec8440834e9cdf038fb34e2d75ac3f87967
Дмитрий <megaamozzg@gmail.com>
2019-10-21 13:19:02 +0300

    Добавил TODO к прошлому комиту

--- app/Notifications/CustomVerifyEmailNotification.php
+++ app/Notifications/CustomVerifyEmailNotification.php
@@ -2,6 +2,7 @@
 
 namespace App\Notifications;
 
+//use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Carbon;
@@ -9,8 +10,11 @@ use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Facades\URL;
 
-class CustomVerifyEmailNotification extends Notification
+class CustomVerifyEmailNotification extends Notification //implements ShouldQueue
 {
+//    TODO: перевести это уведомление на
+//      use Queueable;
+
     /**
      * The callback that should be used to build the mail message.
      *
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,7 +66,7 @@
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
+                                    <i class="fas fa-power-off"></i> {{ __('Выход') }}
                                 </a>
 
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST"

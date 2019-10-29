8f258ec87ef459babd81564b85646e75e7e2da47
Дмитрий <megaamozzg@gmail.com>
2019-10-29 21:45:14 +0300

    Обновление меню

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,10 +59,14 @@
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
+                                {{ Auth::user()->name ?? 'null' }} {{ Auth::user()->fathers_name ?? 'null' }} <span class="caret"></span>
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">
+                                    <i class="fas fa-cogs"></i> {{ __('Настройки') }}
+                                </a>
+                                <hr/>
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">

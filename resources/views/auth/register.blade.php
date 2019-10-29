484c53cf2716905eef6da7e986b5df9caffe1a1d
DimaBDV <46001492+DimaBDV@users.noreply.github.com>
2019-10-29 09:34:02 +0300

    Update fathers_name laber

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -58,7 +58,7 @@
                             {{-- Отчетсво --}}
                             <div class="form-group row">
                                 <label for="fathers_name"
-                                       class="col-md-4 col-form-label text-md-right">{{ __('Отчетсво') }}</label>
+                                       class="col-md-4 col-form-label text-md-right">{{ __('Отчество') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="fathers_name" type="text"
